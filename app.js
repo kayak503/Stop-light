aebd625f840aba3f9ae1eb884a6287fd6264904f
Ari Becker <aribecker503@gmail.com>
2021-11-18 21:50:36 -0500

    testing working copy

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const Gpio = require('onoff').Gpio
 const app = express();
+
+const RED = new Gpio(21, 'out');
+
+app.use(express.urlencoded({extended : false}));
 
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
-    // res.sendFile( __dirname + "/" + "index.htm" );
+    res.sendFile( __dirname + "/html/" + "index.html" );
  })
+ app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + "/html/" + "style.css");
+  });
+  app.get('/main.js', function(req, res) {
+    res.sendFile(__dirname + "/html/" + "main.js");
+  });
 
-
-
- app.listen(3000);
+  app.get('/override:lightState', function(req,res){
+    lightState = req.params.lightState.split(":")[1].split("_")
+    res.send("Data received")
+    RED.writeSync(parseInt(lightState[1]))
+  });
+ 
+  
+ 
+ app.listen(80);
--- html/main.js
+++ html/main.js
@@ -1,10 +1,30 @@
 function Clicked(event){
-    element = event.path[0]
+    let element;
+    try {
+        element = event.path[0];
+    } catch (error) {
+    }
+    try {
+        if (element == undefined){
+            element = event.target;
+        }
+    } catch (error) {
+        console.warn(error)
+    }
+    // Chrome and firefox work
     if (element.className === 'div-button clicked'){ 
         element.className = 'div-button';
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/override:'+element.id+"_1", true);
+        xhr.send(element.id+"_1");
     }
     else{
         element.className = 'div-button clicked';
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/override:'+element.id+"_0", true);
+        xhr.send(element.id+"_0");
     }
     
 }
