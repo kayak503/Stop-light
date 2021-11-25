95bbae0a5da8013c3a68ca3f4ffaaedb03ea7f7d
Ari Becker <aribecker503@gmail.com>
2021-11-25 00:05:06 -0500

    test 05

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const expressEjsLayout = require('express-ejs-layouts');
-//const Gpio = require('onoff').Gpio;
-const app = express();
 
-//const RED = new Gpio(21, 'out');
-//const YELLOW = new Gpio(20, 'out');
-//const GREEN = new Gpio(26, 'out');
+const app = express();
 
 app.set('view engine','ejs');
 app.use(expressEjsLayout);
@@ -18,27 +14,5 @@ app.use('/', express.static(__dirname+ '/')); // any file called from "/" in ejs
 let webApp = require('./routes/webApp');
 app.use('/',webApp("filled"));
 
-/*
-app.get('/', function (req, res) {
-    res.sendFile( __dirname + "/html/" + "index.html" );
- })
- app.get('/style.css', function(req, res) {
-    res.sendFile(__dirname + "/html/" + "style.css");
-  });
-  app.get('/main.js', function(req, res) {
-    res.sendFile(__dirname + "/html/" + "main.js");
-  });
-
-  app.get('/override:lightState', function(req,res){
-    lightState = req.params.lightState.split(":")[1].split("_")
-    res.send("Data received")
-
-    if (lightState[0] == "RED") { RED.writeSync(parseInt(lightState[1]))} 
-    if (lightState[0] == "YELLOW") { YELLOW.writeSync(parseInt(lightState[1]))} 
-    if (lightState[0] == "GREEN") { GREEN.writeSync(parseInt(lightState[1]))} 
-
-  });
- 
-  */
- 
- app.listen(3000);
+// port
+app.listen(3000);
--- routes/webApp.js
+++ routes/webApp.js
@@ -1,42 +1,34 @@
 const express = require('express');
 const router = express.Router();
+
 const Gpio = require('onoff').Gpio;
+
 const RED = new Gpio(21, 'out');
 const YELLOW = new Gpio(20, 'out');
 const GREEN = new Gpio(26, 'out');
 
-module.exports = function (Gpio){
-
-    router.route("/")
-        .get((req, res, next ) =>{
-            res.render('web_index', {red: false ,yellow: false ,green: false});
-            (req, res, next);
-        });
-    router.route("/update/get")
+router.route("/")
     .get((req, res, next ) =>{
-        //res.send("1,0,1")
-
-        let RYG = [
-        RED.readSync().toString(),
-        YELLOW.readSync().toString(),
-        GREEN.readSync().toString()
-        ];
-	let msg = RYG.toString();
-
-        //res.send( (RYG.toString()) )
-        res.send(msg);
-    });
-    router.route("/update/override:lightState")
-    .get((req, res, next ) =>{
-        lightState = req.params.lightState.split(":")[1].split("_")
-        res.send("Data received")
-
-        if (lightState[0] == "RED") { RED.writeSync(parseInt(lightState[1]))} 
-        if (lightState[0] == "YELLOW") { YELLOW.writeSync(parseInt(lightState[1]))} 
-        if (lightState[0] == "GREEN") { GREEN.writeSync(parseInt(lightState[1]))} 
-
+        res.render('web_index', {red: RED.readSync() ,yellow: YELLOW.readSync() ,green:  GREEN.readSync()});
         (req, res, next);
     });
+router.route("/update/get")
+.get((req, res, next ) =>{
+    let RYG = [
+    RED.readSync().toString(),
+    YELLOW.readSync().toString(),
+    GREEN.readSync().toString()
+    ];
+    res.send(RYG.toString());
+});
+router.route("/update/override:lightState")
+.get((req, res, next ) =>{
+    lightState = req.params.lightState.split(":")[1].split("_")
+    res.send("Data received")
+    if (lightState[0] == "RED") { RED.writeSync(parseInt(lightState[1]))} 
+    if (lightState[0] == "YELLOW") { YELLOW.writeSync(parseInt(lightState[1]))} 
+    if (lightState[0] == "GREEN") { GREEN.writeSync(parseInt(lightState[1]))} 
+    (req, res, next);
+});
 
-return router;
-}
+module.exports = router;
